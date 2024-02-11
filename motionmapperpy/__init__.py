1afc5d3ef12b6ee4c917774b69b1c624935cac52
bmarvin <beesemarvin@gmail.com>
2024-02-11 15:20:22 +0100

    cleaned up processing

--- motionmapperpy/__init__.py
+++
@@ -1,7 +0,0 @@
-from .setrunparameters import setRunParameters
-from .mmutils import findPointDensity, gencmap, createProjectDirectory
-from .motionmapper import run_tSne, run_UMAP, runEmbeddingSubSampling, subsampled_tsne_from_projections, findEmbeddings, set_kmeans_model, findClusters
-from .wavelet import findWavelets
-from .wshed import findWatershedRegions
-from .demoutils import makeregionvideo_flies, getTransitions, makeTransitionMatrix, doTheShannonShuffle, \
-    plotLaggedEigenvalues, makeregionvideos_mice
