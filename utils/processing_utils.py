cc8fb317f0d5d4c0ec67eda836eaebff0cde15d8
bmarvin <beesemarvin@gmail.com>
2024-04-11 11:36:19 +0200

    updated the expected folder-structure for the pe-dataset, gitignore includes .vscode for debugging purposes

--- utils/processing_utils.py
+++ utils/processing_utils.py
@@ -13,9 +13,9 @@ def get_directory(is_back=None):
     if is_back is None:
         raise Exception("define kwargs is_back")
     if is_back:
-        return f'{projectPath}/FE_tracks_060000_{BLOCK}/FE_{BLOCK}_060000_back_final'
+        return f'{projectPath}/PE_tracks_final_{BLOCK}/PE_tracks_final_{BLOCK}_back'
     else:
-        return f'{projectPath}/FE_tracks_060000_{BLOCK}/FE_{BLOCK}_060000_front_final'
+        return f'{projectPath}/PE_tracks_final_{BLOCK}/PE_tracks_final_{BLOCK}_front'
 
 
 def get_camera_names(is_back=False):
